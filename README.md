27933513e5bf35d95c230050d07f6a87547013d0
rafala-czytanie <126331152+rafala-czytanie@users.noreply.github.com>
2024-03-02 12:53:20 +0100

    stuff

---
+++ README.md
@@ -0,0 +1,30 @@
+## Game app
+
+To install do the following:
+
+1. Create virtual environment (optional) and launch it.
+
+```
+python -m venv venv/
+source venv/bin/activate
+```
+
+2. Install dependencies - run from project's root dir.
+
+```
+pip install -r requirements.txt
+```
+3. Run app from project's root dir.
+
+```
+python -m game
+```
+
+## Simulation 
+
+To perform actual simulation run:
+
+```
+python -m simulation  # not ready yet
+```
+
---
+++ game.py
@@ -0,0 +1,61 @@
+import pygame
+from src.simulator import Simulator
+from src.shapes.square import Square
+from src.ui import UI
+
+
+class Game:
+    def __init__(self, width, height):
+        pygame.init()
+        self.screen_width = width
+        self.screen_height = height
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        pygame.display.set_caption("Circle Simulation")
+        self.clock = pygame.time.Clock()
+
+        self.colors = {
+            "white": (255, 255, 255),
+            "black": (0, 0, 0),
+            "red": (255, 0, 0),
+            "green": (0, 255, 0)
+        }
+
+        self.font = pygame.font.Font(None, 36)
+
+        self.square = Square(self.screen, self.colors["black"], width // 2, height // 2, 400)
+
+        self.ui = UI(self.screen, self.font, self.colors, 100, 50, 140, 40,
+                     250, 50, 80, 40, self.screen_width, self.screen_height,
+                     250, 90, 80, 40)
+
+        self.simulator = Simulator(self.screen, self.square, self.font, self.colors, self.ui)
+
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                else:
+                    self.ui.handle_event(event)
+
+            if self.ui.button_clicked and self.ui.input_text.isdigit():
+
+                self.simulator.run_simulation(int(self.ui.input_text))
+                self.ui.button_clicked = False
+
+            self.screen.fill(self.colors["white"])
+            self.square.draw()
+
+            self.ui.draw(self.simulator.hit_score,
+                         self.simulator.runs,
+                         self.screen_height)
+
+            pygame.display.flip()
+            self.clock.tick(60)
+        pygame.quit()
+
+
+if __name__ == "__main__":
+    game = Game(900, 700)
+    game.run()
